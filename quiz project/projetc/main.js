b9f8c0a2ddac926789da4ca3648c680c8377970c
swaritk <92188157+swaritk@users.noreply.github.com>
2021-12-26 16:54:34 -0500

    Add files via upload

---
+++ quiz project/projetc/main.js
@@ -0,0 +1,10 @@
+var username = document.getElementById("player1_name_input");
+
+function addUser(){
+    
+    localStorage.setItem(username);
+}
+
+
+
+window.location = "";
---
+++ quiz project/projetc/quizpage.js
@@ -0,0 +1,15 @@
+var username = document.getElementById("player1_name_input");
+
+function addUser(){
+    
+    localStorage.setItem(username);
+    window.location = "gamepage.html";
+}
+
+
+
+
+
+function send() {
+    localStorage.setItem();
+}
